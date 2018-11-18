1147e4b19b02506e732077d9ef303e28013572fc
Joseph Spada <jspada@cs.hartford.edu>
2018-11-18 04:43:54 -0500

    search script DONE

--- resources/scripts/image.js
+++ resources/scripts/image.js
@@ -1,65 +1,68 @@
-// outer container, separates inner img container and outer content from body
-let container = document.createElement("div");
-container.style.cssText =
-    "width: 100%;" +
-    "height: 100%;" +
-    "display: flex;" +
-    "justify-content: center;" +
-    "top: 0;" +
-    "left: 0;" +
-	"z-index: 2;" +
-    "pointer-events: none;" +
-    "position: fixed;";
+// anonymous wrapper because I was sick of thinking of new variable names
+(function(){
+	// outer container, separates inner img container and outer content from body
+	let container = document.createElement("div");
+	container.style.cssText =
+		"width: 100%;" +
+		"height: 100%;" +
+		"display: flex;" +
+		"justify-content: center;" +
+		"top: 0;" +
+		"left: 0;" +
+		"z-index: 2;" +
+		"pointer-events: none;" +
+		"position: fixed;";
 
-// inner container for the image, moves within outer container
-let innerContainer = document.createElement("div");
-innerContainer.style.cssText =
-    "visibility: hidden;" +
-    "width: 95vw;" +
-    "height: 80vh;" +
-    "position: absolute;" +
-    "top: 15vh;" +
-    "transform: translate(0%, -125%);" +
-    "transition: transform 0.75s, visibility 1.5s linear;";
+	// inner container for the image, moves within outer container
+	let innerContainer = document.createElement("div");
+	innerContainer.style.cssText =
+		"visibility: hidden;" +
+		"width: 95vw;" +
+		"height: 80vh;" +
+		"position: absolute;" +
+		"top: 15vh;" +
+		"transform: translate(0%, -125%);" +
+		"transition: transform 0.75s, visibility 1.5s linear;";
 
-// background = the container for the page content (to be blurred)
-// image = the src image of the page image
-let backgrnd;
-let image;
+	// background = the container for the page content (to be blurred)
+	// image = the src of the page image
+	let backgrnd;
+	let image;
 
-// inject all dynamic elements on load
-window.onload = inject;
-function inject(){
-    document.body.appendChild(container);
-    image = document.getElementsByClassName("contentImage")[0];
-    image.addEventListener("click", imgExpand, true);
-    
-    backgrnd = document.getElementById("contentContainer");
-    container.addEventListener("click", imgRetract, true);
+	// inject all dynamic elements on load
+	window.addEventListener("load", inject);
+	function inject(){
+		document.body.appendChild(container);
+		image = document.getElementsByClassName("contentImage")[0];
+		image.addEventListener("click", imgExpand);
+		
+		backgrnd = document.getElementById("contentContainer");
+		container.addEventListener("click", imgRetract);
 
-    let imgSrc = image.getAttribute("src");
-    container.appendChild(innerContainer);
+		let imgSrc = image.getAttribute("src");
+		container.appendChild(innerContainer);
 
-    innerContainer.style.background = "url(" + imgSrc + ") no-repeat center";
-    innerContainer.style.backgroundSize = "contain";
-}
+		innerContainer.style.background = "url(" + imgSrc + ") no-repeat center";
+		innerContainer.style.backgroundSize = "contain";
+	}
 
-// "expand" image (move it down)
-function imgExpand(){
-    innerContainer.style.visibility = "visible";
-    innerContainer.style.transform = "translate(0%, 0)";
-    backgrnd.style.filter = "blur(5px) opacity(0.1%)";
-	container.style.pointerEvents = "auto";
-	image.style.cursor = "auto";
-	container.style.cursor = "zoom-out";
-}
+	// "expand" image (move it down)
+	function imgExpand(){
+		innerContainer.style.visibility = "visible";
+		innerContainer.style.transform = "translate(0%, 0)";
+		backgrnd.style.filter = "blur(5px) opacity(0.1%)";
+		container.style.pointerEvents = "auto";
+		image.style.cursor = "auto";
+		container.style.cursor = "zoom-out";
+	}
 
-// "retract" image (move it up)
-function imgRetract(){
-    innerContainer.style.visibility = "hidden";
-    innerContainer.style.transform = "translate(0%, -125%)";
-    backgrnd.style.filter = "blur(0px) opacity(100%)";	
-	container.style.pointerEvents = "none";
-	image.style.cursor = "zoom-in";
-	container.style.cursor = "auto";
-}
+	// "retract" image (move it up)
+	function imgRetract(){
+		innerContainer.style.visibility = "hidden";
+		innerContainer.style.transform = "translate(0%, -125%)";
+		backgrnd.style.filter = "blur(0px) opacity(100%)";	
+		container.style.pointerEvents = "none";
+		image.style.cursor = "zoom-in";
+		container.style.cursor = "auto";
+	}
+})();
--- resources/scripts/search.js
+++ resources/scripts/search.js
@@ -1,68 +1,112 @@
-// outer container, separates inner img container and outer content from body
-let wrapper = document.createElement("div");
-//let container = document.createElement("div");
-container.style.cssText = //TODO: figure out how this line works when container is never created
-	"background: blue";
+// Google disabled iframe embedding so we had to make use of their CSE API //
+// anonymous wrapper because I was sick of thinking of new variable names
+(function(){
+	// the query to search for, taken from h2 text
+	let query;
+	// contains the returned search result container
+	let container = document.createElement("div");
+	container.style.cssText =
+		"max-height: 0;" +
+		"max-width: 0;" +
+		"resize: none;" +
+		"margin-top: 10px;" +
+		"border: 2px transparent solid;" +
+		"visibility: hidden;" +
+		"filter: blur(7px);" +
+		"transition: visiblity 1s, max-height 1s, max-width 1s, border 1s, filter 0.75s;" +
+		"overflow: auto;";
+	container.id = "searcher";
 	
-// the google search results
-let search = document.createElement("iframe");
-search.style.cssText =
-    "width: 60vw;"+
-	"height: 50vh;"+
-	"background: red;"+
-	"transition: width 1s;";
-
-
-// inject all dynamic elements on load
-window.onload = inject;
-/*function inject(){
-    document.body.appendChild(container);
-    image = document.getElementsByClassName("contentImage")[0];
-    image.addEventListener("click", imgExpand, true);
-    
-    backgrnd = document.getElementById("contentContainer");
-    container.addEventListener("click", imgRetract, true);
-
-    let imgSrc = image.getAttribute("src");
-    container.appendChild(innerContainer);
-
-    innerContainer.style.background = "url(" + imgSrc + ") no-repeat center";
-    innerContainer.style.backgroundSize = "contain";
-}*/
-function inject(){
-	console.log("injection started"); //TODO: delete once fully functional
-	let searchURL = getSearchURL();
-	let contentContainer = document.getElementById("contentContainer");
+	// button to expand/retract search results
+	let btn = document.createElement("button");
+	btn.type = "button";
+	btn.style.cssText =
+		"position: relative;" +
+		"width: 89px;" +
+		"margin: 10px 0 10px 0;" +
+		"padding: 2px;" +
+		"font-size: 11pt;";
+	btn.innerHTML = "Google™ It!";
+	btn.id = "searchBtn";
+	btn.addEventListener("click", searchExpandRetract);
 	
-	container.source = searchURL;
-	container.appendChild(search);
-	contentContainer.insertBefore(container, contentContainer.firstElementChild);
+	// render the search elements needed with specifications for container div, result div, and type of element to render
+	let renderSearchElement = function() {
+		google.search.cse.element.render({
+			div: "searcher",
+			gname: "storesearch",
+			tag: "searchresults-only"
+		});
+		google.search.cse.element.getElement("storesearch").execute(query);
+	};
+
+	// async render of google search elements
+	let render = function() {
+		if (document.readyState == 'complete')
+			renderSearchElement();
+		else
+			google.setOnLoadCallback(renderSearchElement, true);
+	};
+
+	// initialize the google custom search engine and provide callback for after init
+	window.__gcse = {
+		parsetags: 'explicit',
+		callback: render
+	};
+
+	// load the gcse elements and provide the engine address and doc insertion point
+	function loadElements(){
+		let cx = '017154195649547384215:yygtdfgodx8';
+		let gcse = document.createElement('script');
+		gcse.type = 'text/javascript';
+		gcse.async = true;
+		gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+		let search = document.getElementsByTagName('script')[0];
+		search.parentNode.insertBefore(gcse, search);
+	}
+
+	// inject the needed elements like the search container, search elements, and search button
+	function inject(){
+		let title = document.getElementsByTagName("h2")[0];
+		query = title.innerHTML;
+		title.parentNode.parentNode.insertBefore(container, title.parentNode);
+		loadElements();
+		title.parentNode.appendChild(btn);
+	}
 	
-	console.log("injection completed"); //TODO: delete once fully functional
-}
-
-// "expand" image (move it down)
-function imgExpand(){
-    innerContainer.style.visibility = "visible";
-    innerContainer.style.transform = "translate(0%, 0)";
-    backgrnd.style.filter = "blur(5px) opacity(0.1%)";
-	container.style.pointerEvents = "auto";
-	image.style.cursor = "auto";
-	container.style.cursor = "zoom-out";
-}
-
-// "retract" image (move it up)
-function imgRetract(){
-    innerContainer.style.visibility = "hidden";
-    innerContainer.style.transform = "translate(0%, -125%)";
-    backgrnd.style.filter = "blur(0px) opacity(100%)";	
-	container.style.pointerEvents = "none";
-	image.style.cursor = "zoom-in";
-	container.style.cursor = "auto";
-}
-
-function getSearchURL(){
-	let elems = document.getElementsByClassName("contentHeader");
-	let URL = "http://lmgtfy.com/?q=" + elems[0].innerHTML;
-	return URL;
-}
+	// inject everything on load
+	window.addEventListener("load", inject);
+		
+	// expand or retract based on current state (based on text of button)
+	function searchExpandRetract(){
+		btn.disabled = true;
+		if(btn.innerHTML === "Google™ It!"){
+			container.style.border = "2px orangered solid";
+			container.style.maxHeight = "250px";
+			setTimeout(function(){
+				container.style.maxWidth = "90vw";
+				container.style.resize = "both";
+				container.style.filter = "blur(0)";
+			}, 500);
+			container.style.visibility = "visible";
+			setTimeout(function(){
+				btn.innerHTML = "Hide It!";
+				btn.disabled = false;
+			}, 1500);
+		}
+		else{
+			container.style.filter = "blur(7px)";
+			container.style.maxWidth = "0";			
+			setTimeout(function(){	
+				container.style.maxHeight = "0";
+				container.style.resize = "none";
+			}, 1000);
+			setTimeout(function(){
+				container.style.border = "2px transparent solid";
+				container.style.visibility = "hidden";
+				btn.innerHTML = "Google™ It!";
+				btn.disabled = false;
+			}, 2000);
+		}
+	}
+})();
